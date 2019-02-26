59e692116955b8fb18803c3e8903b8823ddf16d5
Janusz <janklyz@gmail.com>
2019-02-26 17:25:01 +0100

    add new item functionality complete

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["1111111","22222@@@@@@@","3333333333333"]
+    var itemArray = ["1111111","22222@@@@@@@","3333333333333"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+// MARK - tableView datasource method
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -26,7 +27,7 @@ class ToDoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
-// tableView delegate metod
+// MARK - tableView delegate metod
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row])
@@ -40,5 +41,30 @@ class ToDoListViewController: UITableViewController {
 
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+// MARK - add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            // what will happen once the user clikcks the add item button on our UIAlert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData() 
+            
+        }
+        
+        alert.addTextField { (alertTextFiled) in
+            alertTextFiled.placeholder = "Create new item"
+            textField = alertTextFiled
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
